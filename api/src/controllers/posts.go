b4a2933b31051d9c5e19380bfb57fd424d7b3b94
Oli Kelly <93444456+olikelly00@users.noreply.github.com>
2024-04-03 08:29:31 +0100

    new commit

--- api/src/controllers/posts.go
+++ api/src/controllers/posts.go
@@ -20,7 +20,17 @@ type JSONPost struct {
 	FileSize  *int64  `json:"image_filesize,omitempty"`
 	FileType  *string `json:"image_filetype,omitempty"`
 	FileData  *[]byte `json:"image_filedata,omitempty"`
-  UserID    string `json:"user_id"`
+	UserID    string  `json:"user_id"`
+	// UserID    string
+	User JSONUser
+	// add fields that would be needed here, important to comm
+	// this to FE
+}
+
+type JSONUser struct {
+	UserID   uint   `json:"user_id"`
+	Username string `json:"username"`
+	Image    []byte `json:"image"`
 }
 
 func GetAllPosts(ctx *gin.Context) {
@@ -44,6 +54,11 @@ func GetAllPosts(ctx *gin.Context) {
 
 	var jsonPosts []JSONPost
 	for _, post := range *posts {
+		user, err := models.FindUser("28")
+		if err != nil {
+			SendInternalError(ctx, err)
+			return
+		}
 		var Filename *string
 		var FileSize *int64
 		var FileType *string
@@ -61,14 +76,19 @@ func GetAllPosts(ctx *gin.Context) {
 			ID:        post.ID,
 			CreatedAt: post.CreatedAt.Format(time.RFC3339),
 			Likes:     post.Likes,
-      UserID:    post.UserID,
 			Filename:  Filename,
 			FileSize:  FileSize,
 			FileType:  FileType,
 			FileData:  FileData,
+			User: JSONUser{
+				UserID:   user.ID,
+				Username: user.Username,
+				Image:    user.FileData,
+			},
 		}
 
 		jsonPosts = append(jsonPosts, jsonPost)
+
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"posts": jsonPosts, "token": token})
@@ -88,6 +108,12 @@ func GetSpecificPost(ctx *gin.Context) {
 		return
 	}
 
+	user, err := models.FindUser("18")
+	if err != nil {
+		SendInternalError(ctx, err)
+		return
+	}
+
 	jsonPost := JSONPost{
 		Message:   post.Message,
 		ID:        post.ID,
@@ -97,7 +123,12 @@ func GetSpecificPost(ctx *gin.Context) {
 		FileSize:  post.FileSize,
 		FileType:  post.FileType,
 		FileData:  post.FileData,
-		UserID:    post.UserID,
+		User: JSONUser{
+			UserID:   user.ID,
+			Username: user.Username,
+			Image:    user.FileData,
+		},
+		//UserID:    post.UserID,
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"post": jsonPost})
@@ -116,6 +147,12 @@ func CreatePost(ctx *gin.Context) {
 		return
 	}
 
+	// val, exists := ctx.Get("userID")
+	// if !exists {
+	// 	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "UserID not found in context"})
+	// 	return
+	// }
+	// userID := val.(string)
 	file, fileHeader, err := ctx.Request.FormFile("image")
 	//defer file.Close()
 	if err != nil {
@@ -147,7 +184,7 @@ func CreatePost(ctx *gin.Context) {
 	postTime := time.Now()
 	likeCount := 0
 	newPost := models.Post{
-    UserID:    userID.(string), // cast the user ID to a string
+		//UserID:    userID, // cast the user ID to a string
 		Message:   message,
 		CreatedAt: postTime,
 		Likes:     likeCount,
@@ -155,23 +192,24 @@ func CreatePost(ctx *gin.Context) {
 		FileSize:  &fileSize,
 		FileType:  &fileType,
 		FileData:  &fileData,
+	}
 
-	PostTime := time.Now()
-	// formattedTime := PostTime.Format("2006-01-02 15:04:05")
-	LikeCount := 0
-	// getting the user id from the gin context and passing an error
-	// if there is none
-	userID, exists := ctx.Get("userID")
-	if !exists {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"ERROR": "USER ID NOT FOUND IN CONTEXT"})
-		return
-	}
-	newPost := models.Post{
-		UserID:    userID.(string), // cast the user ID to a string
-		Message:   requestBody.Message,
-		CreatedAt: PostTime,
-		Likes:     LikeCount,
-	}
+	// PostTime := time.Now()
+	// // formattedTime := PostTime.Format("2006-01-02 15:04:05")
+	// LikeCount := 0
+	// // getting the user id from the gin context and passing an error
+	// // if there is none
+	// userID, exists := ctx.Get("userID")
+	// if !exists {
+	// 	ctx.JSON(http.StatusUnauthorized, gin.H{"ERROR": "USER ID NOT FOUND IN CONTEXT"})
+	// 	return
+	// }
+	// // newPost := models.Post{
+	// // 	UserID:    userID.(string), // cast the user ID to a string
+	// // 	Message:   requestBody.Message,
+	// // 	CreatedAt: PostTime,
+	// // 	Likes:     LikeCount,
+	// // }
 
 	_, err = newPost.Save()
 	if err != nil {
@@ -182,7 +220,6 @@ func CreatePost(ctx *gin.Context) {
 	// val, _ := ctx.Get("userID")
 	// userID := val.(string)
 	// token, _ := auth.GenerateToken(userID)
-
 
 	ctx.JSON(http.StatusCreated, gin.H{"message": "Post created"}) //sends confirmation message back if successfully saved
 }
